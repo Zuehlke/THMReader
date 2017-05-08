4cfeb584ab33c75b886c6b337cde3d30f64c91b2
Jonas Wisplinghoff <jonas.wisplinghoff@zuehlke.com>
2017-05-08 16:52:44 +0200

    Initial Commit

---
+++ THMReader WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  THMReader WatchKit Extension
+//
+//  Created by Jonas Wisplinghoff on 08/05/2017.
+//  Copyright © 2017 Zühlke Engineering GmbH. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
